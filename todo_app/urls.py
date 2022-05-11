e6224bfd8b6fa329c33467f7680d364e9725b0bb
shreya9301 <shreya160730@gmail.com>
2022-05-11 19:18:44 +0530

    comments

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -19,7 +19,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+#swagger interface setup
 schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
@@ -32,11 +32,16 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=[permissions.AllowAny],
 )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # todocontroller api endpoints
     path('api/', include('api.urls')),
+    #authcontroller api endpoints
     path('auth/',include('user_auth.urls')),
+    #swagger 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    #for test purposes only
     path('api-auth/', include('rest_framework.urls')),
 ]
